c310fd5adb11858f2b112c02261311dd7064a0f9
William Torres Dias Dos Santos <58994739+WTDDSSOFT@users.noreply.github.com>
2023-04-29 18:24:23 -0400

    Add autosize Cells and fetchUsers

---
+++ RXswift/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  RXswift
+//
+//  Created by william torres dias dos santos on 29/04/23.
+//
+
+import Foundation
---
+++ RXswift/Model/UserPost.swift
@@ -0,0 +1,8 @@
+//
+//  UserPost.swift
+//  RXswift
+//
+//  Created by william torres dias dos santos on 29/04/23.
+//
+
+import Foundation
